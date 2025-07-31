80508fc329c4fc5ebb5cc1a2000fefd783113f8c
Tanysky89 <pollittt@mail.ru>
2025-07-31 13:40:20 +0400

    Add files via upload

---
+++ Mailing.py
@@ -0,0 +1,11 @@
+class Mailing:
+
+    def __init__(self, to_address, from_address, cost, track):
+        self.to_address = to_address
+        self.from_address = from_address
+        self.cost = cost
+        self.track = track
+
+
+# to_address = Address("443101", "г.Самара", "ул.Ивана Булкина", 45)
+# from_address = Address("443101", "г.Москва", "ул.Ивана Булкина", 45)
---
+++ lesson_3_task_1.py
@@ -0,0 +1,6 @@
+from user import User
+
+my_user = User("Иван", "Петров")
+my_user.PrintLastName()
+my_user.PrintName()
+my_user.PrintFullName()
